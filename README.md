51f7c0bf0dbf82b726d5af5b9665b58ad034e9bb
Paola Zarate GeekGirl <paolazaratem@gmail.com>
2017-08-19 22:01:07 +0200

    add menu and library of charts

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rockkertest
+
+Use AngularJS v1.6.4,
+AngularJS Material Design v1.1.4
+Highcharts JS v5.0.14
+
---
+++ js/app.js
@@ -0,0 +1,70 @@
+var app = angular.module('app', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$timeout', '$mdSidenav', '$log',
+  								function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.toggleLeft = buildDelayedToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+
+    function buildToggler(navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+  }]);
+
+app.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.todos = [{title:"Analytics", icon:"img/analitycs.png"},
+				    {title:"News", icon:"img/newspaper-o.png"},
+				    {title:"Board Brief", icon:"img/board-brief.png"},
+				    {title:"Briefcase", icon:"img/briefcase.png"}
+    ];
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+
+    };
+});
